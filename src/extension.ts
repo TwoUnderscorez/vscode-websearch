0e515af6fe046756f5644e803e145705be14e8f6
TwoUnderscorez <ron.yutkin@gmail.com>
2019-03-23 15:33:53 +0200

    Add a config for multiple search engines in package.json

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,10 @@ import { execute_search } from "./search";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Activating "vscode-websearch"...');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('"vscode-websearch" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+
 	let disposable = vscode.commands.registerCommand('extension.websearch', () => {
 		// The code you place here will be executed every time your command is executed
 		let input = vscode.window.showInputBox();
