ad0f90f277a1b2ccd6111d1995ef16164dc6f6f9
TwoUnderscorez <ron.yutkin@gmail.com>
2019-04-19 14:07:43 +0300

    Acknowledge linux ostype

--- src/urlexec.ts
+++ src/urlexec.ts
@@ -1,8 +1,10 @@
-import { type } from 'os';
+import * as os from 'os';
 import { exec } from 'child_process';
 import { window } from 'vscode';
+import { Config } from "./config";
 
-const ostype: string = type().toLowerCase();
+const ostype: string = os.type().toLowerCase();
+let isLinux: boolean = false;
 
 let open_url = (url: string) => {
     window.showErrorMessage(`${ostype} not supported.`);
@@ -17,6 +19,15 @@ else if (ostype.includes('mac')) {
     open_url = (url: string) => {
         exec(`open https://${url}`);
     };
+}
+else if (ostype.includes('linux')) {
+    open_url = (url: string) => {
+        window.showWarningMessage('Detected linux distro, still loading config...');
+    };
+}
+
+function detect_linux_distro(extconfig: Config) {
+
 }
 
 export { open_url };
